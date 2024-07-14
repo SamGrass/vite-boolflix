30396c5481cadd03f06386fe1b0c012934603fbe
Samgrass <samuelegrassetti01@gmail.com>
2024-07-14 19:38:23 +0200

    fix lista film

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { reactive } from "vue";
 
 export const store = reactive({
     searchText: '',
-    moviesList: 'https://api.themoviedb.org/3/search/movie?api_key=2e8c51d61cce09979ab13516d65193ba&query='
-
+    moviesList: [],
+    moviesURL: 'https://api.themoviedb.org/3/search/movie?api_key=2e8c51d61cce09979ab13516d65193ba&query=',
 
 })
