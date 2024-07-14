bbb803d12fe0408825838cd94a118da155388c5d
Samgrass <samuelegrassetti01@gmail.com>
2024-07-14 19:35:33 +0200

    aggiunta api film

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 import { reactive } from "vue";
 
 export const store = reactive({
+    searchText: '',
+    moviesList: 'https://api.themoviedb.org/3/search/movie?api_key=2e8c51d61cce09979ab13516d65193ba&query='
 
 
 })
